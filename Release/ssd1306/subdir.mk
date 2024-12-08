4a7c9f99915be6695495549dc0a03a179193351f
wayp123 <77548714+wayp123@users.noreply.github.com>
2024-12-08 18:03:52 +0000

    Initial commit

---
+++ Release/ssd1306/subdir.mk
@@ -0,0 +1,33 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (12.3.rel1)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../ssd1306/ssd1306.c \
+../ssd1306/ssd1306_fonts.c \
+../ssd1306/ssd1306_tests.c 
+
+OBJS += \
+./ssd1306/ssd1306.o \
+./ssd1306/ssd1306_fonts.o \
+./ssd1306/ssd1306_tests.o 
+
+C_DEPS += \
+./ssd1306/ssd1306.d \
+./ssd1306/ssd1306_fonts.d \
+./ssd1306/ssd1306_tests.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+ssd1306/%.o ssd1306/%.su ssd1306/%.cyclo: ../ssd1306/%.c ssd1306/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m7 -std=gnu11 -DUSE_HAL_DRIVER -DSTM32F767xx -c -I../Core/Inc -I../Drivers/STM32F7xx_HAL_Driver/Inc -I../Drivers/STM32F7xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32F7xx/Include -I../Drivers/CMSIS/Include -Os -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv5-d16 -mfloat-abi=hard -mthumb -o "$@"
+
+clean: clean-ssd1306
+
+clean-ssd1306:
+	-$(RM) ./ssd1306/ssd1306.cyclo ./ssd1306/ssd1306.d ./ssd1306/ssd1306.o ./ssd1306/ssd1306.su ./ssd1306/ssd1306_fonts.cyclo ./ssd1306/ssd1306_fonts.d ./ssd1306/ssd1306_fonts.o ./ssd1306/ssd1306_fonts.su ./ssd1306/ssd1306_tests.cyclo ./ssd1306/ssd1306_tests.d ./ssd1306/ssd1306_tests.o ./ssd1306/ssd1306_tests.su
+
+.PHONY: clean-ssd1306
+
